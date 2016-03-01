0c1d2f6a0dbba09ada542e1baa2d73f150bc6b6f
Matthew Moen <matt@kilpatrickdesign.com>
2016-03-01 17:16:23 -0500

    OK those don't work, so drop them.

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+sysctl -w net.core.somaxconn=4096
+
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+  chown -R redis .
+  exec gosu redis "$BASH_SOURCE" "$@"
+fi
+
+exec "$@"
